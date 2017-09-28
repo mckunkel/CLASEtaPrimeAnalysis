8ba0e437a44530e83c5aab3eaa0d011411fc9d52
mckunkel <mkunkel@jlab.org>
2017-09-28 21:14:09 +0200

    ready for full sim with GEMC prt4

--- deleteWorkFlows.pl
+++ deleteWorkFlows.pl
@@ -11,13 +11,13 @@ use warnings;
 
 my @torusValue = ("-0.75", "0.75", "1.0", "-1.0");
 my @solenoidValue = ("0.6", "0.8");
-
+my $deletePrefix = "machineLearning";
 
 
 for $a (0 .. $#torusValue)
 {
   for $b (0 .. $#solenoidValue){
-	  my $rm_workflow = "swif cancel etaP_sim_tor".$torusValue[$a]."sol".$solenoidValue[$b]." -delete";
+	  my $rm_workflow = "swif cancel ".$deletePrefix."_sim_tor".$torusValue[$a]."sol".$solenoidValue[$b]." -delete";
     system($rm_workflow);
     print "$workflow \n";
    
